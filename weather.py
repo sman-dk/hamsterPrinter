3d5e60f401e9616eafc6eaaec574a75e78514ae6
Georg Sluyterman <georg@sman.dk>
2018-01-12 00:36:02 +0100

    forecastWeather data in support works

--- weather.py
+++ weather.py
@@ -7,9 +7,9 @@ import sys
 import atexit
 import time
 import json
+import requests
 import MySQLdb
 import schedule
-from WeatherAPIXU import Weather_APIXU
 
 # Argument parsing
 
@@ -44,12 +44,12 @@ def exit_handler():
     conn.close()
 atexit.register(exit_handler)
 
-
 # Load weather config
 keys = ['apixuApiKey', 'location', 'chuteLength']
 weatherConf = {}
 for k in keys:
     weatherConf[k] = cfg.get('weather', k)
+weatherConf['apixu_api_url']='http://api.apixu.com/v1/'
 # optional parameters
 keys = ['currentEnable', 'currentEnforceUpdateInterval', 
     'currentEnforceUpdateTime', 'ForecastEnable',
@@ -60,13 +60,15 @@ for k in keys:
         weatherConf[k] = cfg.get('weather', k)
     except:
         pass
+
 # Calculate chutePx
 chuteLengthPx = int(int(weatherConf['chuteLength']) / 2.54 * int(cfg.get('printer', 'verticalDpi')))
 
 def weatherCurrent(weatherConf):
     try:
-        w = Weather_APIXU(weatherConf['apixuApiKey'])
-        jsonObject = json.dumps(w.weather_current(query=weatherConf['location']))
+        jsonObject = json.dumps(requests.get(weatherConf['apixu_api_url'] + 
+            'current.json?key=' + weatherConf['apixuApiKey'] + 
+            '&q=' + weatherConf['location'], timeout=30 ).json())
         dbWeather = conn.cursor()
         dbWeather.execute("""INSERT INTO printout (srcType, jdoc) VALUES (
             (SELECT id FROM srcType WHERE shortName = "WeatherCurrent") , %s)""" , (jsonObject,))
@@ -79,8 +81,9 @@ def weatherCurrent(weatherConf):
 
 def weatherForecast(weatherConf):
     try:
-        w = Weather_APIXU(weatherConf['apixuApiKey'])
-        jsonObject = json.dumps(w.weather_forecast(query=weatherConf['location'], days=weatherConf['ForecastDays']))
+        jsonObject = json.dumps(requests.get(weatherConf['apixu_api_url'] + 
+            'forecast.json?key=' + weatherConf['apixuApiKey'] + 
+            '&q=' + weatherConf['location'] + '&days=' + weatherConf['ForecastDays'], timeout=30 ).json())
         dbWeather = conn.cursor()
         dbWeather.execute("""INSERT INTO printout (srcType, jdoc) VALUES (
             (SELECT id FROM srcType WHERE shortName = "WeatherForecast") , %s)""" , (jsonObject,))
@@ -88,28 +91,7 @@ def weatherForecast(weatherConf):
     except Exception, e:
         print(e)
     else:
-        print "Inserted a weather update of type WeatherForecast into the database."
-
-def weatherStuffUnfinished():
-    try:
-        dbWeather = conn.cursor()
-        jsonObject = json.dumps({
-            "name": "hest",
-            "screen_name": "hest",
-            "created_at": "hest",
-            "timestamp_ms": "hest",
-            "url": "hest",
-            "text": "hest",
-            "hashtags": "hest",
-            "is_quote_status": "hest",
-            "retweet": "hest",
-            "urlPics": "hest"})
-        dbWeather.execute("""INSERT INTO printout (srcType, jdoc) VALUES (
-            (SELECT id FROM srcType WHERE shortName = "WeatherForecast") , %s)""" , (jsonObject,))
-        dbWeather.close()
-    except Exception, e:
-        print(e)
-
+        print "Inserted a weather update of type WeatherCurrent into the database."
 
 if "currentEnable" in weatherConf:
     if "currentEnforceUpdateInterval" in weatherConf:
@@ -133,8 +115,8 @@ while True:
         if hamster.pinAgain("WeatherCurrent", conn, chuteLengthPx, cfg):
             weatherCurrent(weatherConf)
     if "ForecastEnable" in weatherConf:
-        #if hamster.pinAgain("WeatherForecast", conn, chuteLengthPx, cfg):
-            #weatherForecast(weatherConf)
+        if hamster.pinAgain("WeatherForecast", conn, chuteLengthPx, cfg):
+            weatherForecast(weatherConf)
         pass
 
     time.sleep(10)
